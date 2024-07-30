aca5125f77efe9b414e90367afc521a096254cc8
Vitalii Demchyshyn <demchyshyn.v87@gmail.com>
2024-07-30 22:02:50 +0200

    lesson1-hw

---
+++ src/main/java/org/example/jan2024springhw/Jan2024SpringHwApplication.java
@@ -0,0 +1,13 @@
+package org.example.jan2024springhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jan2024SpringHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jan2024SpringHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jan2024springhw/Jan2024SpringHwApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jan2024springhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jan2024SpringHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
